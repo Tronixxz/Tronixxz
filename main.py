af4c8514523f1aef8ebe379934cf76726832160f
Hsb1122 <153083377+Hsb1122@users.noreply.github.com>
2024-02-10 23:06:31 +0500

    added new file

---
+++ main.py
@@ -0,0 +1,169 @@
+# my_name : str = 'Haseeb'
+
+# print(my_name)
+
+# length : int = 6
+# width : int = 4
+# print(length*width)
+
+# per : float = 43.1
+# print(per)
+
+# h : int = 6
+# w : int = 4
+# print(f"""the height is {h}
+#  and width is {w}
+#   finally area is {h} * {w} = {h*w}""")
+
+# #    In Operator
+
+# name : str = "Haseeb"
+# print("a" in name)
+
+# n1 : int = 2
+# n2 : int = 3
+# # print(id(n1))
+# # print(id(n2))
+# print(n1 is n2)
+
+# IF ELSE
+
+
+# age = int(input('Please enter your age: '))
+
+# if age >= 18:
+#     print('You are eligible to vote.')
+# else:
+#     print('You are not eligible to vote.')
+
+# 2nd attemp
+
+# age = int(input('Please enter your age:'))
+
+# if age >= 18:
+#     print('you are eligible for vote.')
+# else:
+#         print('not eligible')
+
+
+# For loop
+
+# name = 'haseeb'
+# for i in name:
+#     print(i)
+
+
+# Question
+
+# colors = ['red', 'blue','green','yellow']
+# for color in colors:
+#   print(color)
+#   for i in color:
+#     print(i)
+
+# 1 feb practice LOOPS
+
+# rows = 5
+
+
+# for i in range(1, rows + 1):
+
+#     for j in range(rows - i):
+#         print(" ", end="")
+
+
+#     for k in range(2 * i - 1):
+#         print("*", end="")
+
+
+#     print()
+
+
+# age : int = int(input("Please enter your age:"))
+# if age >= 18:
+#   print("You are eligible for vote")
+# else:
+#   print("you are not eligible for vote")
+
+
+# for i in range(1,6):
+#   print(i)
+
+# prime : int = int(input("what is your prime minister name: "))
+# ans : int = "imran khan"
+
+# if prime == ans:
+#     print("yes you are right")
+# else:
+#     print("you are wrong")
+
+
+# prime = input("What is your prime minister's name: ")
+# ans = "imran khan"
+
+# if prime == ans:
+#     print("Yes, you are right!")
+# else:
+#     print("You are wrong.")
+
+
+# prime = input("Please enter the prime minister name:")
+# name : int = "imran khan"
+# while(prime != name):
+#     print("sorry you are wrong")
+#     prime = input("Please enter the prime minister name:")
+# else:
+#     print("yes you are right")
+
+# prime = input("Please enter the prime minister's name: ")
+# name = "imran khan"
+
+# while prime.lower() != name:
+#     print("Sorry, you are wrong.")
+#     prime = input("Please enter the prime minister's name: ")
+# else:
+#     print("Yes, you are right.")
+
+
+# my_name : int = "Haseeb"
+# for i in my_name:
+#     print(i)
+
+# age: int = int(input("Please enter your age:"))
+# while (age <= 18):
+#     print("you are elible for vote")
+#     age: int = int(input("Please enter your age:"))
+# else:
+#     print("sorry you are not eligible for vote")
+
+
+# vowels = (input("Please enter the letter:"))
+# while(vowels in ['a','e','i','o','u']):
+#     print("its vowels")
+# else:
+#     print("its not")
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(2,21,2):
+#     print(i)
+
+# my_name = "haseeb"
+# for i in my_name:
+#     print(i)
+
+
+# fruits = ["apple", "banana", "cherry", "date"]
+# for i in fruits:
+#     print(i)
+
+
+
+
+
+
+
+
+
+ksfhej;ldkw;sl
---
+++ pra.py
@@ -0,0 +1,115 @@
+# myname : int = "Haseeb"
+# for i in myname:
+#     print(i)
+
+# age = int(input('Please enter your age: '))
+
+# if age >= 18:
+#     print('You are eligible to vote.')
+# else:
+#     print('You are not eligible to vote.')
+
+
+# For loop use in table.........................................................
+
+# no : int = int(input("Please enter the range:"))
+# table_no : int = int(input("please enter table no:"))
+# for i in range(1,no+1):
+#   print(f"{table_no} x {i} = {table_no * i}")
+
+
+# no : int = int(input("please enter the range:"))
+# table_no : int = int(input("please enter table number:"))
+# for i in range(1,no+1):
+#     print(f"{table_no} x {i} = {table_no * i}")
+
+# While Loop......................................................................
+
+# n : str = 'yes'
+# While (n == "yes")
+# no : int = int(input("please enter the range:"))
+# table_no : int = int(input("please enter table number:"))
+# for i in range(1,no+1):
+#     print(f"{table_no} x {i} = {table_no * i}")
+# n = input('Enter yes or no:')
+# if 'no':
+#     break
+
+# while True:
+#     n = input('Enter yes or no: ')
+
+#     if n.lower() == 'no':
+#         break
+
+#     if n.lower() != 'yes':
+#         print('Invalid input. Please enter yes or no.')
+#         continue
+
+#     no = int(input("Please enter the range: "))
+#     table_no = int(input("Please enter table number: "))
+
+#     for i in range(1, no + 1):
+#         print(f"{table_no} x {i} = {table_no * i}")
+
+
+# Break And Continue .............................................
+
+# for i in range(1,10+1):
+
+#     if i == 5:
+#         continue
+#     print(i)
+
+
+# for i in range(1,10+1):
+
+#     if i == 5:
+#         break
+#     print(i)
+
+
+# Prize bond with for loop.................................................
+
+# for i in range(1,1023+1):
+#     if i == 1022:
+#         print(f"Congrats you won prize bond and your refral number is {i}")
+#         break
+#     else:
+#         print(f"sorry we could not find your refral number")
+
+
+# Prize bond with While loop.....................................................abs
+
+# Prize_bond_no : int = int(input("please enter your prize bond number:"))
+# bond_no : int = 40
+# while(Prize_bond_no!=bond_no):
+#     print(f"sorry we couldnot find your bond number")
+#     Prize_bond_no : int = int(input("please enter your prize bond number:"))
+# else:
+#     print(f"congrats you won prize bond")
+
+
+# Prize_bond_no : int = int(input("please enter your number:"))
+# bond_no : int = 12
+# while (Prize_bond_no!=bond_no):
+#     print(f"sorry")
+#     Prize_bond_no : int = int(input("please enter your number:"))
+# else:
+#     print(f"you won")
+
+# Break and continue.....................................................................////////////////////...............]
+
+
+
+
+# Make a calculate..................................IMPORTANT
+
+
+# equation = input("Enter a equation:")
+# print(eval(equation))
+
+
+# Make a calemder 
+# import calendar
+
+# print(calendar.calendar(2023))
